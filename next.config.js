d03348213a3ac9144bfa5e9c05d28e94bb1b1d58
Salim Emmanuel Raji <salimraji@icloud.com>
2025-09-04 09:32:32 +0300

    fix: Correct basePath for GitHub Pages styling

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  basePath: isProd ? "/SalimRajiPortfolio" : "",
-  assetPrefix: isProd ? "/SalimRajiPortfolio/" : "",
+  basePath: isProd ? "/Portfolio" : "",
+  assetPrefix: isProd ? "/Portfolio/" : "",
   output: "export",
   images: {
     unoptimized: true,
